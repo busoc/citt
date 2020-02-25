811b62253497c1f5f60afaaf58c4f521766532a8
midbel <brun.nicolas21@gmail.com>
2020-02-25 13:26:18 +0100

    cleaning code

--- main.go
+++ main.go
@@ -3,64 +3,50 @@ package main
 import (
 	"bufio"
 	"crypto/md5"
+	"errors"
 	"flag"
+	"fmt"
 	"io"
-	"log"
 	"os"
-	"path/filepath"
 
 	"golang.org/x/sync/errgroup"
 )
 
 const CITT = 0xFFFF
 
-var (
-	Version   string = ""
-	BuildTime string = ""
-)
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "usage: citt [-h] <file...>")
+		os.Exit(2)
+	}
+}
 
 func main() {
-	log.SetFlags(0)
-	log.SetOutput(os.Stderr)
-
-	version := flag.Bool("version", false, "print version and exit")
 	parallel := flag.Int("parallel", 10, "parallel task")
-	flag.Usage = func() {
-		log.Printf("usage: %s [-version] <file...>", filepath.Base(os.Args[0]))
-		os.Exit(1)
-	}
 	flag.Parse()
-	if *version {
-		log.Printf("%s version %s (%s)", filepath.Base(os.Args[0]), Version, BuildTime)
-		os.Exit(1)
-	}
-	if flag.NArg() == 0 {
-		flag.Usage()
-	}
-	var g errgroup.Group
 
 	if *parallel <= 0 {
 		*parallel = 1
 	}
-	sema := make(chan struct{}, *parallel)
+	var (
+		sema = make(chan struct{}, *parallel)
+		grp  errgroup.Group
+	)
 	for _, a := range flag.Args() {
-		a := a
 		sema <- struct{}{}
-		g.Go(func() error {
-			s, bs, err := Calculate(a)
+		a := a
+		grp.Go(func() error {
+			citt, sum, err := Calculate(a)
 			<-sema
-			if err != nil {
-				log.Fatalln(err)
-			}
 			if err != nil {
 				return err
 			}
-			log.Printf("%s: crc: %#x (%[2]d) - md5: %x", a, s, bs)
+			fmt.Printf("%s: crc: %#x (%[2]d) - md5: %x\n", a, citt, sum)
 			return nil
 		})
 	}
-	if err := g.Wait(); err != nil {
-		log.Fatalln(err)
+	if err := grp.Wait(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
@@ -70,11 +56,13 @@ func Calculate(p string) (uint16, []byte, error) {
 		return CITT, nil, err
 	}
 	defer f.Close()
-	w := md5.New()
 
-	r := io.TeeReader(f, w)
+	var (
+		w = md5.New()
+		r = io.TeeReader(f, w)
+	)
 	v, err := calculate(r)
-	if err != nil && err != io.EOF {
+	if err != nil && !errors.Is(err, io.EOF) {
 		return v, nil, err
 	}
 	s := w.Sum(nil)
